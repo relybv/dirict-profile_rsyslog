ce4e415b15775f3b32d048505994d89e6aa8a58e
Paul Gomersbach <p.gomersbach@rely.nl>
2016-07-26 20:22:36 +0000

    use rsyslog class

--- manifests/install.pp
+++ manifests/install.pp
@@ -15,19 +15,8 @@ class profile_rsyslog::install {
     require => User[ 'syslog'],
   }
 
-  class { 'rsyslog::server':
-    enable_tcp                => true,
-    enable_udp                => true,
-    enable_relp               => false,
-    enable_onefile            => false,
-    server_dir                => '/srv/log/',
-    custom_config             => undef,
-    port                      => '514',
-    address                   => '*',
-    high_precision_timestamps => false,
-    log_templates             => false,
-    actionfiletemplate        => false,
-    require                   => File['/srv/log'],
+  class { 'rsyslog':
+    preserve_fqdn => true,
   }
 
 }
